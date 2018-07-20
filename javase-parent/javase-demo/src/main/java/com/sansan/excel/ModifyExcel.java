ee9821ef78c94c95869ead879cd0b93f051a220c
xiapf <xiapf@asiainfo.com>
2018-07-20 16:52:05 +0800

    修改

--- javase-parent/javase-demo/src/main/java/com/sansan/excel/ModifyExcel.java
+++ javase-parent/javase-demo/src/main/java/com/sansan/excel/ModifyExcel.java
@@ -1,19 +1,19 @@
 package com.sansan.excel;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.junit.Test;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class ModifyExcel {
-    public static void main(String[] args) throws IOException {
+    @Test
+    public void testExcelExport() throws IOException {
         //读取Excel模板
-        FileInputStream excelInputStream = new FileInputStream("E:/跨省数字预堪提交集团模板.xls");
+        InputStream excelInputStream = this.getClass().getResourceAsStream("SurveyInfoTemplate.xls");
+//        FileInputStream excelInputStream = new FileInputStream(name);
 
         //创建2003版的Excel
         Workbook workbook = new HSSFWorkbook(excelInputStream);
@@ -24,10 +24,13 @@ public class ModifyExcel {
         //获取Excel文件的第一个Sheet页
         Sheet sheet = workbook.getSheetAt(0);
 
-        Row row = sheet.createRow(2);
-        Cell cell = row.createCell(0);
-        cell.setCellValue("2");
+        Row row1 = sheet.getRow(1);
+        sheet.removeRow(row1);
 
+        Row row = sheet.getRow(1);
+        if (null == row) {
+            System.out.println("removeCRow后，该Row为null");
+        }
 
         FileOutputStream excelOutPutStream = new FileOutputStream("E:/跨省数字预堪提交集团模板-副本.xls");
         workbook.write(excelOutPutStream);
